4354256f314a5f701b4e34e1386bdbad8f5ff750
berkahhariini999-rgb <berkahhariini999@gmail.com>
2025-11-06 08:51:10 +0700

    Initial Commit

---
+++ ComponentOnboardingPage/ComponentOnboardingPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentOnboardingPageApp.swift
+//  ComponentOnboardingPage
+//
+//  Created by Iqbal Alhadad on 06/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ComponentOnboardingPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
